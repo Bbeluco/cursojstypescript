ed292b383800a6c425cc915a01e0db569adead72
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-02-16 16:06:05 -0300

    Create A0024-super.ts

---
+++ src/A0024-super/A0024-super.ts
@@ -0,0 +1,50 @@
+export class Pessoa {
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+    private idade: number,
+    protected cpf: string,
+  ) {}
+
+  getIdade(): number {
+    return this.idade;
+  }
+
+  getCpf(): string {
+    return this.cpf;
+  }
+
+  getNomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+/*A palavra super serve para acessar a super classe
+  é notavel que a classe aluno é uma herança de pessoa
+  Logo, utilizamos o super para acessar a classe superior no caso Pessoa
+*/
+
+export class Aluno extends Pessoa {
+  constructor(
+    nome: string,
+    sobrenome: string,
+    idade: number,
+    cpf: string,
+    public sala: string,
+  ) {
+    super(nome, sobrenome, idade, cpf);
+  }
+}
+export class Cliente extends Pessoa {
+  getNomeCompleto(): string {
+    return 'Isso vem do cliente: ' + this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoa = new Pessoa('Bruno', 'Beluco', 30, '000.000.000-00');
+const aluno = new Aluno('Bruno', 'Beluco', 30, '000.000.000-00');
+const cliente = new Cliente('Bruno', 'Beluco', 30, '000.000.000-00');
+
+console.log(pessoa.getNomeCompleto());
+console.log(aluno.getNomeCompleto());
+console.log(cliente.getNomeCompleto());
